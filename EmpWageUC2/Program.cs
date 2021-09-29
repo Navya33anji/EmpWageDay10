c66b63757ccd34612b1d3a8fe795f1e95fe1017d
NAVYATHA MEKALA <navyatha487@gmail.com>
2021-09-29 18:06:41 +0530

    Daily emp Wage

---
+++ EmpWageUC2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Emp_Wage_UC2_Day8
+{
+  public  class Program
+    {
+        static void Main(string[] args)
+        {
+            UC2_Empwage.EmpWage();
+        }
+    }
+}
